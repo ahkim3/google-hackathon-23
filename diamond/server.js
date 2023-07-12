7f25191d20cef017f3199f689d46b83cc7ed0a6d
Andrew Kim <andrewhkim3@gmail.com>
2023-07-11 20:14:13 -0500

    added server js file

---
+++ diamond/server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello world!");
+});
+
+app.get("/add", function (req, res) {
+    // Store GET params
+    process.env.RECEIVER_ID = req.query.receiverid;
+    process.env.AMOUNT = req.query.amount;
+    process.env.DURATION = req.query.duration;
+
+    // Run batch script
+    const child_process = require("child_process");
+    const command = "ls -l";
+    const output = child_process.execSync(command);
+    console.log(output);
+
+    res.send("GET Request Received");
+});
+
+app.listen(80);
