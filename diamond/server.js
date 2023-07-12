d0aa48e01863f89ea2f3fe0430b60d2c28085208
Andrew Kim <andrewhkim3@gmail.com>
2023-07-11 22:09:58 -0500

    update server

--- diamond/server.js
+++ diamond/server.js
@@ -7,15 +7,23 @@ app.get("/", function (req, res) {
 
 app.get("/add", function (req, res) {
     // Store GET params
+    console.log(req.query.receiverid);
+    console.log(req.query.amount);
+    console.log(req.query.duration);
+
     process.env.RECEIVER_ID = req.query.receiverid;
     process.env.AMOUNT = req.query.amount;
     process.env.DURATION = req.query.duration;
 
-    // Run batch script
+    // Run commands
     const child_process = require("child_process");
-    const command = "npx hardhat run scripts/deploy.js --network goerli";
+    const command = "npx hardhat compile";
     const output = child_process.execSync(command);
     console.log(output);
+
+    const command2 = "npx hardhat run scripts/deploy.js --network goerli";
+    const output2 = child_process.execSync(command2);
+    console.log(output2);
 
     res.send("GET Request Received");
 });
