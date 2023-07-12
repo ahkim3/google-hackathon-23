dc157976e6c357912d44cb2e87687b956b26a264
Andrew Kim <andrewhkim3@gmail.com>
2023-07-11 22:38:10 -0500

    fix spelling again

--- diamond/hardhat.config.js
+++ diamond/hardhat.config.js
@@ -1,21 +1,21 @@
 /**
-* @type import('hardhat/config').HardhatUserConfig
-*/
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 
-require('dotenv').config();
+require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 
-const { API_URL, PRIVATE_KEY, SENDER_ID, RECIEVER_ID, AMOUNT, DURATION } = process.env;
+const {API_URL, PRIVATE_KEY, SENDER_ID, RECEIVER_ID, AMOUNT, DURATION} =
+    process.env;
 
 module.exports = {
-   solidity: "0.7.3",
-   defaultNetwork: "goerli",
-   networks: {
-      hardhat: {},
-      goerli: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
-      }
-   },
-}
-
+    solidity: "0.7.3",
+    defaultNetwork: "goerli",
+    networks: {
+        hardhat: {},
+        goerli: {
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
+        },
+    },
+};
